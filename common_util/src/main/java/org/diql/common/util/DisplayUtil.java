89bf366a7b51c8735be2ef47048ff65649df6955
qinglian <diqinglian@gmail.com>
2018-05-05 17:52:27 +0800

    init;

---
+++ common_util/src/main/java/org/diql/common/util/DisplayUtil.java
@@ -0,0 +1,60 @@
+package org.diql.common.util;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by qinglian on 2018/5/5.
+ */
+public class DisplayUtil {
+    private static final String TAG = "DisplayUtil";
+
+    public static final float IOS_DP_SCALE = 360 / 375;
+
+    private DisplayUtil() {
+        // empty.
+    }
+
+    public static float dip2pxFloat(@NonNull Context context, final float dipValue) {
+//        checkNull(context);
+        return context.getResources().getDisplayMetrics().density * dipValue;
+    }
+
+    public static int dip2pxInt(@NonNull Context context, final float dipValue) {
+//        checkNull(context);
+        return (int) (context.getResources().getDisplayMetrics().density * dipValue + 0.5);
+    }
+
+    public static float px2dipFloat(@NonNull Context context, final float pxValue) {
+        return pxValue / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static int px2dipInt(@NonNull Context context, final float pxValue) {
+        return (int) (pxValue / context.getResources().getDisplayMetrics().density + 0.5);
+    }
+
+    public static float sp2pxFloat(@NonNull Context context, final float spValue) {
+        return spValue * context.getResources().getDisplayMetrics().scaledDensity;
+    }
+
+    public static int sp2pxInt(@NonNull Context context, final float spValue) {
+        return (int) (spValue * context.getResources().getDisplayMetrics().scaledDensity + 0.5);
+    }
+
+    public static float px2spFloat(@NonNull Context context, final float pxValue) {
+        return pxValue / context.getResources().getDisplayMetrics().scaledDensity;
+    }
+
+    public static int px2spInt(@NonNull Context context, final float pxValue) {
+        return (int) (pxValue / context.getResources().getDisplayMetrics().scaledDensity + 0.5);
+    }
+
+    public static float iOSdip2pxFloat(@NonNull Context context, final float iOSdipValue) {
+        return dip2pxFloat(context, iOSdipValue) * IOS_DP_SCALE;
+    }
+
+    public static int iOSdip2pxInt(@NonNull Context context, final float iOSdipValue) {
+        return (int) (iOSdip2pxFloat(context, iOSdipValue)+ 0.5);
+    }
+
+}
---
+++ common_util/src/test/java/org/diql/common/util/DisplayUtilTest.java
@@ -0,0 +1,12 @@
+package org.diql.common.util;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by qinglian on 2018/5/5.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class DisplayUtilTest {
+    // empty.
+}
