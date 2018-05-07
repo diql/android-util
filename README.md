ccc1f9e02012abb40d42aa0832f65787cb11fb1d
qinglian <diqinglian@gmail.com>
2018-05-07 22:30:27 +0800

    README: dependency;

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# android-util
-Android utils lib. 
+# android util
+Android utils lib.
+
+## dependency
+maven:
+```
+<dependency>
+  <groupId>org.diql</groupId>
+  <artifactId>common-util</artifactId>
+  <version>0.1.0</version>
+  <type>pom</type>
+</dependency>
+```
+
+gradle:
+```groovy
+compile 'org.diql:common-util:0.1.0'
+```
--- app/src/main/java/org/diql/util/MainActivity.java
+++ app/src/main/java/org/diql/util/MainActivity.java
@@ -2,6 +2,9 @@ package org.diql.util;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.diql.common.util.DisplayUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int px = DisplayUtil.dip2pxInt(this, 10);
+        Toast.makeText(this, "10px:" + px, Toast.LENGTH_SHORT).show();
     }
 }
